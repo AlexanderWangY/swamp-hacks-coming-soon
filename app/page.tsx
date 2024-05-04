98aebe88d804cea837955e29d2c0a5008ee31bf8
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-05-04 00:05:21 -0500

    weird stuffs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 'use client';
+import Spline from '@splinetool/react-spline';
 
 export default function Home() {
   return (
     <div className='flex h-screen w-screen flex-row overflow-hidden'>
-      <div className='flex w-1/2 flex-col items-center justify-center border'>
-        <div className='mb-10 flex h-fit w-4/5 flex-col border'>
+      <div className='ml-14 flex w-1/2 flex-col items-center justify-center'>
+        <div className='mb-10 flex h-fit w-4/5 flex-col'>
           <div>
             <h1 className='text-11xl font-medium text-white'>SWAMP</h1>
             <h1 className='mt-[-5.5%] text-11xl font-medium tracking-tighter text-zinc-400'>
@@ -14,10 +15,13 @@ export default function Home() {
         </div>
 
         <div className='w-4/5'>
-          <button className='rounded-xl bg-white pb-4 pl-10 pr-10 pt-4 text-xl text-black transition-all hover:rounded-sm'>
+          <button className='rounded-xl bg-white pb-4 pl-10 pr-10 pt-4 text-xl text-black shadow-sm shadow-white transition-all hover:scale-110 hover:rounded-sm hover:shadow-lg'>
             Sponsor Us
           </button>
         </div>
+      </div>
+      <div className='grow border border-red-500'>
+        <Spline scene='https://prod.spline.design/4ea23GDD3GLvXnlU/scene.splinecode' />
       </div>
     </div>
   );
