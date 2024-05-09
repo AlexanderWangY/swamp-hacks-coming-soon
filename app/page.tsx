ebe88e8088e12d01258192a503d7fb4ff6550688
AlexanderWangY <alexander.yisu.wang@outlook.com>
2024-05-09 09:37:17 -0500

    working on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,12 @@
 import Image from 'next/image';
 import DiscordIcon from '../public/logos/discord.svg';
 import { useAnimate } from 'framer-motion';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 export default function Home() {
   const [scope, animate] = useAnimate();
+  const isPhone = useMediaQuery({ maxWidth: 767 });
 
   // Move animated things to different file (TODO)
   const animateIntro = async () => {
@@ -160,7 +162,7 @@ export default function Home() {
   return (
     <div
       ref={scope}
-      className='relative flex h-screen w-screen flex-col items-center justify-center overflow-hidden'
+      className='relative flex h-screen w-screen flex-col items-center overflow-hidden sm:justify-center'
     >
       <h1
         id='x-text'
@@ -173,11 +175,11 @@ export default function Home() {
           id='modal'
           className='border-padding flex items-center justify-center rounded-full bg-gradient-to-r from-green-500 from-20% via-purple-600 via-80% to-pink-500 opacity-0'
         >
-          <p className='h-full w-full rounded-full bg-black px-4 py-2 text-lg text-white'>
+          <p className='h-full w-full rounded-full bg-black px-4 py-2 text-sm text-white sm:text-lg'>
             10th Anniversary
           </p>
         </div>
-        <div className='mb-5 flex select-none flex-col items-center justify-center text-11xl text-white'>
+        <div className='sm: mb-5 flex select-none flex-col items-center justify-center text-white md:text-8xl lg:text-11xl'>
           <h1 id='swamp' className='mb-[-5%] font-semibold opacity-0'>
             SWAMP
           </h1>
@@ -185,23 +187,37 @@ export default function Home() {
             HACKS
           </h1>
         </div>
-        <p id='subtext' className='mb-10 text-xl text-zinc-300 opacity-0'>
-          Florida&apos;s Flagship Hackathon{' '}
-          <span className='tracking-tighter text-zinc-500'>&lt;/&gt;</span>{' '}
-          Gainesville, FL{' '}
-          <span className='tracking-tighter text-zinc-500'>&lt;/&gt;</span>{' '}
-          <span className='from-green-500 from-20% via-purple-600 via-80% to-pink-500'>
-            Coming Spring 2025
-          </span>
-        </p>
 
-        <div className='flex w-2/3 flex-row justify-evenly'>
+        {isPhone ? (
+          <div
+            id='subtext'
+            className='mb-10 flex flex-col items-center justify-center text-lg text-zinc-300 opacity-0 sm:text-lg md:text-xl'
+          >
+            <p>Florida&apos;s Flagship Hackathon</p>
+            <p>Gainesville, FL</p>
+            <span className='from-green-500 from-20% via-purple-600 via-80% to-pink-500'>
+              Coming Spring 2025
+            </span>
+          </div>
+        ) : (
+          <div id='subtext' className='mb-10 text-xl text-zinc-300 opacity-0'>
+            Florida&apos;s Flagship Hackathon{' '}
+            <span className='tracking-tighter text-zinc-500'>&lt;/&gt;</span>{' '}
+            Gainesville, FL{' '}
+            <span className='tracking-tighter text-zinc-500'>&lt;/&gt;</span>{' '}
+            <span className='from-green-500 from-20% via-purple-600 via-80% to-pink-500'>
+              Coming Spring 2025
+            </span>
+          </div>
+        )}
+
+        <div className='flex w-5/6 flex-col items-center justify-evenly border sm:w-2/3 sm:flex-row'>
           <button
             onClick={() => {
               window.location.href = 'mailto:info@swamphacks.com';
             }}
             id='sponsor-button'
-            className='w-44 rounded-md bg-white px-5 py-3 text-xl text-black opacity-0 shadow-[] transition-all hover:bg-zinc-300'
+            className='w-full rounded-md bg-white px-5 py-3 text-xl text-black opacity-0 shadow-[] transition-all hover:bg-zinc-300 sm:w-44'
           >
             Sponsor Us
           </button>
@@ -210,7 +226,7 @@ export default function Home() {
               window.open('https://discord.swamphacks.com', '_blank')
             }
             id='discord-button'
-            className='flex w-44 flex-row items-center justify-evenly rounded-md border border-zinc-800 bg-zinc-950 px-5 py-3 text-xl text-white opacity-0 transition-all hover:border-zinc-700'
+            className='flex w-full flex-row items-center justify-evenly rounded-md border border-zinc-800 bg-zinc-950 px-5 py-3 text-xl text-white opacity-0 transition-all hover:border-zinc-700 sm:w-44'
           >
             Discord
             <Image
